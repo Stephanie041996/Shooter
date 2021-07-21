72813860068cb90a0ddb08bf2be003589759076b
Stephanie Michelle Sakuhuni <michellesakuhuni5@gmail.com>
2021-07-21 15:33:06 +0200

    Add Scenes for game

---
+++ src/SceneMain.js
@@ -0,0 +1,14 @@
+import Phaser from './phaser';
+
+export default class SceneMain extends Phaser.Scene {
+    constructor() {
+      super({ key: "SceneMain" });
+    }
+   preload(){
+       
+   }
+    create() {
+      
+    }
+  }
+  
